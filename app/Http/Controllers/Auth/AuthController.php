9a56569d21e1ab0e3c9d70d1cf4e0b4a4c1f0bf5
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2022-11-09 20:14:50 -0500

    update AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -38,13 +38,11 @@ class AuthController extends Controller
     public function login(Request $request)
     {
       
-
         $user = User::where('email', $request['email'])->firstOrFail();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()
-            ->json(['message' => 'Hi '.$user->name.', welcome to home','access_token' => $token, 'token_type' => 'Bearer', ]);
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer' ]);
     }
 
     // method for user logout and delete token
@@ -53,7 +51,7 @@ class AuthController extends Controller
         auth()->user()->tokens()->delete();
 
         return [
-            'message' => 'You have successfully logged out and the token was successfully deleted'
+            'message' => ':)'
         ];
     }
 }
